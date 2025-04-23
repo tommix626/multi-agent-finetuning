58f7cd1a9ff2930e181c298ba714277813b699c3
tommix626 <xwang397@jh.edu>
2025-04-23 17:27:09 -0400

    modify expert agent

--- models/expert_agent.py
+++ models/expert_agent.py
@@ -18,13 +18,20 @@ class ExpertAgent:
         """Return perplexity under this adapter."""
         raise NotImplementedError()
 
-    def get_training_loss_on(self, QA_data: QADatapoint, lr: float):
+    def get_training_loss_on(self, QA_data: QADatapoint, lr: float, record_training=True):
         """Get training this expert on a single datapoint."""
+        #TODO: Activate the adapter, forward pass, then query the log prob. Follow Hw6/Hw7
         raise NotImplementedError()
 
     def reset_adapter(self):
         """Reset adapter weights (e.g., if overfitted)."""
-        raise NotImplementedError()
+        # fetch the current setting
+        adapter_config = self.adapter.peft_config
+        tokenizer = self.adapter.tokenizer
+        device = self.adapter.device
+
+        # restart an adapter from scratch
+        self.adapter = ExpertAdapter(self.peft_model, adapter_config, f"adapter_for_expert_{self.id}", tokenizer, device, force_new=True)
 
     def record_datapoint(self, datapoint_id: str):
         self.training_data_freq[datapoint_id] += 1
