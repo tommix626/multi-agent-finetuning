d0107ccccbfbbda14ce7aca3d4f9d944fa4aa6e6
tommix626 <xwang397@jh.edu>
2025-04-23 15:31:19 -0400

    (ppl) init expert cluster logic

---
+++ models/expert_cluster.py
@@ -0,0 +1,48 @@
+"""
+The Expert Cluster manages the Expert Agents.
+The base model is in control of the expert cluster, and the agent only provide adaptors to be load and unload (using PEFT).
+
+"""
+
+from typing import List, Tuple
+
+from peft import PeftConfig, PeftModel, peft_model
+from transformers.models.auto.modeling_auto import AutoModelForCausalLM
+from transformers.models.auto.tokenization_auto import AutoTokenizer
+
+from models.expert_adapter import ExpertAdapter
+from models.expert_agent import ExpertAgent
+import transformers
+
+class ExpertCluster:
+    def __init__(self, base_model_name: str, peft_config: PeftConfig, num_experts: int, device: str):
+        self.peft_config = peft_config
+        self.peft_model, self.tokenizer = self._setup_peft_model(base_model_name)
+
+        self.experts: List[ExpertAgent] = []
+        self._init_agent(num_experts)
+
+        self.device = device
+
+    def _setup_peft_model(self, base_model_name: str) -> Tuple[PeftModel, AutoTokenizer]:
+        """setup the base model and return the model."""
+        base_model= AutoModelForCausalLM.from_pretrained(base_model_name)
+        tokenizer = AutoTokenizer.from_pretrained(base_model_name)
+
+        return PeftModel(base_model, self.peft_config), tokenizer
+
+
+    def _init_agent(self, num_agents:int):
+        # init experts
+        for i in range(num_agents):
+            self.experts.append(ExpertAgent(i, self.peft_model, self.peft_config, self.tokenizer, self.device))  # expert themselves manage the adapter logics.
+
+
+    def delegate_and_assign(self, row):
+        perplexities = [exp.compute_perplexity(row["text"]) for exp in self.experts]
+        chosen_expert_id = self._select(perplexities)
+        return chosen_expert_id
+
+    def _select(self, perplexities: List[int]) -> int:
+        """delegation strategy for which expert to train on"""
+
