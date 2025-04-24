de57ec37ed7e5ad8647b925cf6e002093b206377
tommix626 <xwang397@jh.edu>
2025-04-23 20:08:51 -0400

    impl perplexity get

--- models/expert_agent.py
+++ models/expert_agent.py
@@ -1,4 +1,5 @@
 from peft import PeftConfig, PeftModel
+import torch
 from transformers.models.auto.tokenization_auto import AutoTokenizer
 from data.datamodel import QADatapoint
 
@@ -11,12 +12,32 @@ class ExpertAgent:
         self.id = expert_id
         self.peft_model = base_peft_model  # shared reference
         self.training_data_freq = defaultdict(int)
+        self.device = device
 
         self.adapter = ExpertAdapter(self.peft_model, adapter_config, f"adapter_for_expert_{self.id}", tokenizer, device)
 
-    def compute_perplexity(self, QA_data: QADatapoint) -> float:
+    def compute_perplexity(self, batch: dict) -> float:
         """Return perplexity under this adapter."""
-        raise NotImplementedError()
+        self.adapter.activate()
+        self.peft_model.eval()
+        
+        with torch.no_grad():
+            input_ids = batch['input_ids'].to(self.device)
+            attention_mask = batch['attention_mask'].to(self.device)
+            labels = batch['input_ids'].clone().to(self.device)
+
+            outputs = self.peft_model(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                labels=labels
+            )
+
+            loss = outputs.loss
+            perplexity = torch.exp(loss).item()
+
+        return perplexity
+
+
 
     def get_training_loss_on(self, QA_data: QADatapoint, lr: float, record_training=True):
         """Get training this expert on a single datapoint."""
