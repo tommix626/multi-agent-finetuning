7b2b070f1eb06fd41b4763ad92426f904593dbfd
tommix626 <xwang397@jh.edu>
2025-04-26 15:17:49 -0400

    (ppl) modify temperature and perplexity selection

--- models/expert_cluster.py
+++ models/expert_cluster.py
@@ -8,6 +8,7 @@ import random
 from typing import List, Tuple
 
 from peft import PeftConfig, PeftModel, peft_model
+import torch
 from transformers.models.auto.modeling_auto import AutoModel, AutoModelForCausalLM
 from transformers.models.auto.tokenization_auto import AutoTokenizer
 
@@ -16,7 +17,7 @@ from models.expert_agent import ExpertAgent
 import transformers
 
 class ExpertCluster:
-    def __init__(self, base_model: PeftModel, tokenizer:AutoTokenizer, peft_config: PeftConfig, num_experts: int, device: str):
+    def __init__(self, base_model: PeftModel, tokenizer:AutoTokenizer, peft_config: PeftConfig, num_experts: int, device: str, selection_temperature:float):
         self.peft_config = peft_config
         self.peft_model, self.tokenizer = base_model, tokenizer
         # self.peft_model, self.tokenizer = self._setup_peft_model(base_model_name)
@@ -24,6 +25,9 @@ class ExpertCluster:
 
         self.experts: List[ExpertAgent] = []
         self._init_agent(num_experts)
+
+        self.selection_temperature = selection_temperature
+
 
 
     def _setup_peft_model(self, base_model_name: str) -> Tuple[PeftModel, AutoTokenizer]:
@@ -50,6 +54,8 @@ class ExpertCluster:
     should incorporate some regularization (randomness). As per Daniel's suggestion.
         """
         perplexities = [-p for p in perplexities]
+        perplexity_tensor = torch.tensor(perplexities)
+        scores = -perplexity_tensor / self.selection_temperature
         index = [i for (i,_) in enumerate(perplexities)]
         return random.choices(index, perplexities)[0]
 
